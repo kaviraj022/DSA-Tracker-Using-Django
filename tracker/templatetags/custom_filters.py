77bce4cc57a5b40ece279f54f4a97326009ffc7d
kaviraj022 <ikavikr8098@gmail.com>
2025-05-29 12:14:49 +0530

    Completed all functionalities for both admin and user

--- tracker/templatetags/custom_filters.py
+++ tracker/templatetags/custom_filters.py
@@ -4,4 +4,19 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(key, 0) 
+    return dictionary.get(key, 0)
+
+@register.filter
+def split(value, delimiter=','):
+    """Split a string into a list using the given delimiter."""
+    return value.split(delimiter)
+
+@register.filter
+def get_difficulties(problems):
+    """Get unique difficulties from problems."""
+    return sorted(set(item['difficulty'] for item in problems))
+
+@register.filter
+def filter_by_difficulty(problems, difficulty):
+    """Filter problems by difficulty level."""
+    return [item for item in problems if item['difficulty'] == difficulty] 
