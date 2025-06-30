be78abed0977c225a980a51ae808029f4bb30e74
kaviraj022 <ikavikr8098@gmail.com>
2025-06-30 13:48:32 +0530

    updated for railway deployment

--- dsa_tracker/settings.py
+++ dsa_tracker/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tracker/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
